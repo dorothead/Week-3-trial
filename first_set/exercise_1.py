e7d4167c5634af44f47c868c439967dd516c2f40
Vincent Velthuizen <mail@vincentvelthuizen.com>
2020-03-05 15:29:38 +0100

    exercise 1 basic version

---
+++ first_set/exercise_1.py
@@ -0,0 +1,16 @@
+day_int = int(input("What day of the week is it? (number please)"))
+
+if day_int == 0:        # note the double "="-sign
+    print("Sunday")
+elif day_int == 1:
+    print("Monday")
+elif day_int == 2:
+    print("Tuesday")
+elif day_int == 3:
+    print("Wednesday")
+elif day_int == 4:
+    print("Thursday")
+elif day_int == 5:
+    print("Friday")
+else:
+    print("Saturday")
