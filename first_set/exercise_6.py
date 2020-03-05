dd12f4ffa5790cb7b8b500ffbc43f0969a68cbe9
Vincent Velthuizen <mail@vincentvelthuizen.com>
2020-03-05 15:55:00 +0100

    exercise 6

---
+++ first_set/exercise_6.py
@@ -0,0 +1,17 @@
+numbers = [83, 75, 74.9, 70, 69.9, 65, 60, 59.9, 55, 50,49.9, 45, 44.9, 40, 39.9, 2, 0]
+
+for grade in numbers:
+    if grade >= 75:
+        print("First")
+    elif grade >= 70:
+        print("Upper second")
+    elif grade >= 60:
+        print("Second")
+    elif grade >= 50:
+        print("Third")
+    elif grade >= 45:
+        print("F1 Supp")
+    elif grade >= 40:
+        print("F2")
+    else:
+        print("F3")
